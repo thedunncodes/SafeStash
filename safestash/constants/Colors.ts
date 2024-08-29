9a8c0995552d6defb10972fd8234eba31bafc95b
MoMoAxe <tobioluwa348@gmail.com>
2024-08-29 03:23:17 +0100

    UPDATE: Colors to be used for styling

---
+++ safestash/constants/Colors.ts
@@ -0,0 +1,14 @@
+const background = '#edf2f4'
+const textColor = '#2b2d42'
+const red = '#d90429'
+
+const Colors = {
+    light: {
+        background,
+        text: textColor,
+        logo: red,
+        icons: red,
+    }
+}
+
+export default Colors;
