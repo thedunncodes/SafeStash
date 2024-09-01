068c314da056c27fdad25754ebb802b916866a39
MoMoAxe <tobioluwa348@gmail.com>
2024-09-01 02:09:38 +0100

    UPDATE: register screen added to app layout

--- safestash/app/_layout.tsx
+++ safestash/app/_layout.tsx
@@ -4,6 +4,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" />
+      <Stack.Screen name="(onboarding)/register" options={{ title: 'Register' }} />
     </Stack>
   );
 }
