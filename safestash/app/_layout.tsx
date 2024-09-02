149403de1ec37b40bba5f6e4573afc4b2e12c81e
MoMoAxe <tobioluwa348@gmail.com>
2024-09-02 21:24:23 +0100

    UPDATE: added new fonts to be used in app

--- safestash/app/_layout.tsx
+++ safestash/app/_layout.tsx
@@ -1,10 +1,18 @@
 import { Stack } from "expo-router";
+import { useFonts } from 'expo-font'
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    PoppinsSemiBold: require('../assets/fonts/Poppins-SemiBold.ttf'),
+    PoppinsBold: require('../assets/fonts/Poppins-Bold.ttf'),
+    PoppinsExtraBold: require('../assets/fonts/Poppins-ExtraBold.ttf'),
+    PoppinsMedium: require('../assets/fonts/Poppins-Medium.ttf'),
+  });
+
   return (
-    <Stack>
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
-      <Stack.Screen name="(onboarding)/register" options={{ title: 'Register' }} />
+      <Stack.Screen name="(onboarding)" options={{ title: 'Register' }} />
     </Stack>
   );
 }
