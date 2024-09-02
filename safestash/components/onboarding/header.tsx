96d069660ba86fc1d49cd639825bb90ca1661119
MoMoAxe <tobioluwa348@gmail.com>
2024-09-02 21:29:37 +0100

    UPDATE: header component to simulate web 'header' tag

---
+++ safestash/components/onboarding/header.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Colors from '@/constants/Colors';
+import { View, Text, Platform, StyleSheet, StatusBar, type ViewProps } from 'react-native';
+
+interface HeaderProps extends ViewProps {
+    // children: React.ReactNode;
+    context?: string
+}
+
+export default function Header({style,  context, ...rest}: HeaderProps ) {
+    return (
+        <View style={ styles.header } >
+            <Text style={[ styles.text, style ]} {...rest} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        backgroundColor: Colors.light.background,
+        marginTop: 5,
+        alignItems: 'flex-start',
+        // borderWidth: 1,
+        // borderColor: 'red',
+        padding: 0,
+    },
+    text: {
+        fontSize: 23,
+        fontFamily: 'PoppinsBold',
+        color: Colors.light.text,
+        // borderWidth: 1,
+        // borderColor: 'blue',
+    }
+})
