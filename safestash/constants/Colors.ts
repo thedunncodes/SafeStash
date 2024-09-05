8939c05914945563fa999ab531625ecf5e3dadd6
MoMoAxe <tobioluwa348@gmail.com>
2024-09-05 22:37:26 +0100

    UPDATE: new color added

--- safestash/constants/Colors.ts
+++ safestash/constants/Colors.ts
@@ -1,4 +1,5 @@
 const background = '#edf2f4'
+const backgroundTransparent = 'rgba(237, 242, 244, .8)'
 const textColor = '#2b2d42'
 const red = '#d90429'
 const darkRed = 'rgb(184, 0, 0)'
@@ -9,6 +10,7 @@ const Colors = {
         text: textColor,
         logo: red,
         icons: red,
+        tBg: backgroundTransparent,
         red,
         darkRed,
     },
