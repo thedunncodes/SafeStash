2c2bbc65809de51cebe7bea56447da29244caf87
MoMoAxe <tobioluwa348@gmail.com>
2024-09-05 22:42:07 +0100

    UPDATE: testing form inputs

--- safestash/app/(onboarding)/register.tsx
+++ safestash/app/(onboarding)/register.tsx
@@ -1,11 +1,14 @@
-import { Text, View, Platform, StyleSheet } from 'react-native';
+import { Text, View, Platform, TouchableWithoutFeedback, StyleSheet, Keyboard } from 'react-native';
 import { useEffect } from 'react';
 import { useNavigation, Link } from "expo-router";
 import Colors from '@/constants/Colors';
 import BodyView from '@/components/bodyView';
 import Header from '@/components/onboarding/header';
+import FormInput from '@/components/formInput';
+import { useAppState } from '@/components/appStates/onboardingFormStates';
 
 export default function Reg() {
+    const { firstName, setFirstName, countryCode, mobileNumber, setMobileNumber} = useAppState()
 
     return (
         <BodyView style={styles.body}>
@@ -16,6 +19,13 @@ export default function Reg() {
                 Let's Create your account
             </Header>
             <Text style={styles.infoSection} >Please input your E-mail and phone number to begin the process </Text>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <View style={{ flex: 1 }}>
+                    <FormInput placeholder='test1' secureText={false} onChangeText={setFirstName} value={firstName} type='default' keyboardType='default' />
+                    <FormInput placeholder='Country Code' secureText={false} onChangeText={setMobileNumber} type='code' value={mobileNumber} codeValue={countryCode} keyboardType='numeric' />
+                    <View><Text> Results: Hello {firstName} from {countryCode} your number is {countryCode} {mobileNumber} </Text></View>
+                </View>
+            </TouchableWithoutFeedback>
         </BodyView>
     )
 }
