8256f68fca5d4310f89a008a42f0074bd3d0428e
MoMoAxe <tobioluwa348@gmail.com>
2024-09-08 08:25:05 +0100

    UPDATE: splash screen

--- safestash/app/_layout.tsx
+++ safestash/app/_layout.tsx
@@ -1,6 +1,10 @@
 import { Stack } from "expo-router";
+import { useEffect } from 'react';
 import { useFonts } from 'expo-font';
 import { StateProvider } from "@/components/appStates/onboardingFormStates";
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded] = useFonts({
@@ -9,6 +13,23 @@ export default function RootLayout() {
     PoppinsExtraBold: require('../assets/fonts/Poppins-ExtraBold.ttf'),
     PoppinsMedium: require('../assets/fonts/Poppins-Medium.ttf'),
   });
+
+  async function Prepare() {
+    useEffect(() => {
+      if (loaded) {
+        setTimeout(() => {
+          SplashScreen.hideAsync();
+        }, 1500);
+        
+      }
+    }, [loaded]);
+  
+    if (!loaded) {
+      return null;
+    }
+  }
+
+  Prepare()
 
   return (
     <StateProvider>
