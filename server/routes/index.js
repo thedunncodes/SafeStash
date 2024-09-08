ff7ca1ca5e252d5ff3490d6a0ac4dcdd9b333266
thedunncodes <tobioluwa348@gmail.com>
2024-09-08 07:16:01 +0100

    UPDATE: added a 'verify' route and also validation with express validator

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import AppController from '../controllers/AppController';
+import FormController from '../controllers/FormController';
 
 const router = Router();
 
 router.get('/', AppController.home);
+
+router.post('/verify', body('mobileNumber').isMobilePhone('any', { strictMode: true }), FormController.verify);
 
 module.exports = router;
