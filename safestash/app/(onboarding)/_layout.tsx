2e20127b7d06579a9d3e3fa1dcdbc1079d6a6d94
MoMoAxe <tobioluwa348@gmail.com>
2024-09-10 07:02:54 +0100

    UPDATE: added to new screens, userData and personalization

--- safestash/app/(onboarding)/_layout.tsx
+++ safestash/app/(onboarding)/_layout.tsx
@@ -5,6 +5,8 @@ export default function OnboardingLayout() {
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="register" options={{ title: 'Register' }} />
       <Stack.Screen name="verification" options={{ title: 'Verification' }} />
+      <Stack.Screen name="userData" options={{ title: 'Profile Data' }} />
+      <Stack.Screen name="personalization" options={{ title: 'Personalization' }} />
     </Stack>
   );
 }
