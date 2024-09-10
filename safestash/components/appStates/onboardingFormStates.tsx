70dc675ee00b9d3e2ef6f3d3b274b2064ed31b92
MoMoAxe <tobioluwa348@gmail.com>
2024-09-10 06:59:02 +0100

    UPDATE: added new states and also code refractor

--- safestash/components/appStates/onboardingFormStates.tsx
+++ safestash/components/appStates/onboardingFormStates.tsx
@@ -4,11 +4,12 @@ export interface formValidation {
   email?: string,
   code?: string,
   mobileNumber?: string,
+  newJob?: string
 }
 
 type AppStateContextType = {
-  firstName: string;
-  setFirstName: React.Dispatch<React.SetStateAction<string>>;
+  lastName: string;
+  setLastName: React.Dispatch<React.SetStateAction<string>>;
   givenName: string;
   setGivenName: React.Dispatch<React.SetStateAction<string>>;
   email: string;
@@ -17,12 +18,16 @@ type AppStateContextType = {
   setMobileNumber: React.Dispatch<React.SetStateAction<string>>;
   countryCode: string;
   setCountryCode: React.Dispatch<React.SetStateAction<string>>;
+  country: string;
+  setCountry: React.Dispatch<React.SetStateAction<string>>;
   password: string;
   setPassword: React.Dispatch<React.SetStateAction<string>>;
   date: Date;
   setDate: React.Dispatch<React.SetStateAction<Date>>;
   dateField: string;
   setDateField: React.Dispatch<React.SetStateAction<string>>;
+  occupation: string;
+  setOccupation: React.Dispatch<React.SetStateAction<string>>;
   errors: formValidation;
   setErrors: React.Dispatch<React.SetStateAction<formValidation>>;
 };
@@ -30,37 +35,32 @@ type AppStateContextType = {
 const AppStateContext = createContext<AppStateContextType | undefined>(undefined);
 
 export const StateProvider = ({ children }: { children: ReactNode }) => {
-  const [firstName, setFirstName] = useState<string>('');
+  const [lastName, setLastName] = useState<string>('');
   const [givenName, setGivenName] = useState<string>('');
   const [email, setEmail] = useState<string>('');
   const [mobileNumber, setMobileNumber] = useState<string>('');
   const [countryCode, setCountryCode] = useState<string>('+_ _');
+  const [country, setCountry] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [date, setDate] = useState<Date>(new Date())
   const [dateField, setDateField] = useState<string>('')
+  const [occupation, setOccupation] = useState<string>('')
   const [errors, setErrors] = useState<formValidation>({})
 
 
   return (
     <AppStateContext.Provider value={{
-        firstName,
-        setFirstName,
-        givenName,
-        setGivenName,
-        email,
-        setEmail,
-        mobileNumber,
-        setMobileNumber,
-        countryCode,
-        setCountryCode,
-        password,
-        setPassword,
-        date,
-        setDate,
-        dateField,
-        setDateField,
-        errors,
-        setErrors
+        lastName, setLastName,
+        givenName, setGivenName,
+        email, setEmail,
+        mobileNumber, setMobileNumber,
+        countryCode, setCountryCode,
+        country, setCountry,
+        password, setPassword,
+        date, setDate,
+        dateField, setDateField,
+        errors, setErrors,
+        occupation, setOccupation
     }}>
       {children}
     </AppStateContext.Provider>
