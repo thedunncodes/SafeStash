18a2333d9793b9cd472c61998300c135382fdbb2
thedunncodes <tobioluwa348@gmail.com>
2024-09-15 00:12:21 +0100

    UPDATE: List containing available PFI dids

---
+++ server/utils/pfiList.js
@@ -0,0 +1,8 @@
+const pfiList = {
+  AquaFiDid: 'did:dht:3fkz5ssfxbriwks3iy5nwys3q5kyx64ettp9wfn1yfekfkiguj1y',
+  FlowBackFiDid: 'did:dht:zkp5gbsqgzn69b3y5dtt5nnpjtdq6sxyukpzo68npsf79bmtb9zy',
+  VertexFiDid: 'did:dht:enwguxo8uzqexq14xupe4o9ymxw3nzeb9uug5ijkj9rhfbf1oy5y',
+  TitaniumFiDid: 'did:dht:ozn5c51ruo7z63u1h748ug7rw5p1mq3853ytrd5gatu9a8mm8f1o',
+};
+
+export default pfiList;
