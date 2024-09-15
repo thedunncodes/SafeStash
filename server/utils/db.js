29be9382cdc35e0a7d65b722d8f5368a2f14541a
thedunncodes <tobioluwa348@gmail.com>
2024-09-15 13:21:10 +0100

    UPDATE: added to new postgresql table to store user accounts and record transactions

--- server/utils/db.js
+++ server/utils/db.js
@@ -13,7 +13,7 @@ const pool = new Pool({
 // use int not serial for user_id
 async function createTables() {
   await pool.query(`
-    CREATE TABLE IF NOT EXISTS user_profile(
+    CREATE TABLE IF NOT EXISTS user_profile (
       user_id SERIAL PRIMARY KEY,
       last_name VARCHAR(30),
       given_name VARCHAR(80),
@@ -26,9 +26,25 @@ async function createTables() {
       email VARCHAR(30) UNIQUE,
       password VARCHAR(40)
       );
+    CREATE TABLE IF NOT EXISTS accounts (
+      account_id SERIAL PRIMARY KEY,
+      user_id INT REFERENCES user_profile (user_id),
+      balance DECIMAL(12, 2) NOT NULL DEFAULT 0.00,
+      currency VARCHAR(3) NOT NULL,
+      user_did VARCHAR(60) NOT NULL UNIQUE,
+      user_VC TEXT[],
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    CREATE TABLE IF NOT EXISTS transactions (
+      transaction_id SERIAL PRIMARY KEY,
+      account_id INT REFERENCES accounts(account_id),
+      amount DECIMAL(12, 2) NOT NULL,
+      transaction_type VARCHAR(10) NOT NULL, -- 'credit' or 'debit'
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
     `);
 
-  console.log('Database tables setup');
+  console.log('Database tables Created');
 }
 
 createTables();
