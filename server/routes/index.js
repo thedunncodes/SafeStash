75d6cc33dc36ce22b86bc101c8dfd3cd8f370ed0
thedunncodes <tobioluwa348@gmail.com>
2024-09-15 13:19:31 +0100

    UPDATE: added '/offerings', 'credentials' and '/sendRfq' end points to integrate with tbdex

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { body } from 'express-validator';
 import AppController from '../controllers/AppController';
 import FormController from '../controllers/FormController';
+import TbdDexController from '../controllers/TbdDexController';
 
 const router = Router();
 
@@ -18,5 +19,11 @@ router.post('/tags', FormController.tags);
 router.post('/register', FormController.register);
 
 router.post('/login', FormController.login);
+
+router.get('/offerings', TbdDexController.getOfferings);
+
+router.post('/sendRfq', TbdDexController.sendRfq);
+
+router.post('/credentials', TbdDexController.credentials);
 
 module.exports = router;
